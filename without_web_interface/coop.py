1bdd61c334b9c2778b1692f879dffcb7d4de3a8e
mappleton <mappleton@gmail.com>
2021-10-19 10:45:19 -0700

    Add files via upload

---
+++ without_web_interface/coop.py
@@ -0,0 +1,119 @@
+'''
+Chicken coop automation w/o web interface for raspberry pi.
+Opens and closes coop door and tunnel door based on sunrise & sunset.
+
+Author: Michael Appleton
+Date: September 26, 2021
+
+'''
+
+import RPi.GPIO as GPIO
+import time
+import astral
+import datetime
+from astral.sun import sun
+from apscheduler.schedulers.background import BackgroundScheduler
+import logging
+import logging.handlers as handlers
+
+# Define logger to add events to coop.log
+logger = logging.getLogger('coop')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+logHandler = handlers.RotatingFileHandler('/home/pi/coop_without_web/coop.log', maxBytes=1000000, backupCount=2)
+logHandler.setLevel(logging.INFO)
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
+# Create schedule handler
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+# Define the location to determine event times
+city = astral.LocationInfo(name='Edmonds, WA', region='USA', timezone='US/Pacific', latitude=47.8107, longitude=-122.3774)
+
+
+GPIO.setmode(GPIO.BCM)
+
+# Create a dictionary called pins to store the pin number, name, and pin state
+pins = {
+   23 : {'name' : 'Door Close', 'state' : GPIO.HIGH},
+   24 : {'name' : 'Door Open', 'state' : GPIO.HIGH},
+   5 : {'name' : 'Tunnel Open', 'state' : GPIO.HIGH},
+   6 : {'name' : 'Tunnel Close', 'state' : GPIO.HIGH}
+   }
+
+# Set each pin as an output and make it low
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.HIGH)
+
+#class for the door functions.  Coop/tunnel, open/close
+class Door:
+    
+    # parameterized constructor
+    def __init__(self, door, direction, duration, gpio_pin):
+        self.door = door
+        self.direction = direction
+        self.duration = duration
+        self.gpio_pin = gpio_pin
+     
+
+    def door_run(self):
+        GPIO.output(self.gpio_pin, GPIO.LOW)
+        time.sleep(self.duration)
+        GPIO.output(self.gpio_pin, GPIO.HIGH)
+        logger.info(str(self.door)+'-'+str(self.direction))
+
+
+
+ 
+# creating object of the class
+# Door('which door', 'open/close', duration in seconds, pin #)
+coop_open = Door('coop', 'open', 35, 24)
+coop_close = Door('coop', 'close', 50, 23) 
+tunnel_open = Door('tunnel', 'open', 40, 5)
+tunnel_close = Door('tunnel', 'close', 40, 6)
+
+
+
+# Function to add events to coop.log
+def log_events(x):
+    jobs = scheduler.get_jobs()
+    logger.info(x)
+    for job in jobs:
+        logger.info('scheduled - '+str(job))
+
+
+# Function to add the daily events to the scheduler
+def add_events():
+    # Needs wifi to get actual sunrise/sunset.  Opening tunnel 30 min after coop door
+    try:
+        sunrise = astral.sun.sunrise(city.observer, date=datetime.date.today(), tzinfo=city.timezone)
+        dusk = astral.sun.dusk(city.observer, date=datetime.date.today(), tzinfo=city.timezone)
+        tunnel_open_time = sunrise + datetime.timedelta(minutes=30)
+        scheduler.add_job(coop_open.door_run, 'date', run_date=sunrise)
+        scheduler.add_job(tunnel_open.door_run, 'date', run_date=tunnel_open_time)
+        scheduler.add_job(coop_close.door_run, 'date', run_date=dusk)
+        scheduler.add_job(tunnel_close.door_run, 'date', run_date=dusk)
+        log_events('add_events had wifi')
+
+
+    except:
+        fixeddawn = datetime.datetime.combine(datetime.datetime.today(),datetime.time(7,00))
+        fixeddusk = datetime.datetime.combine(datetime.datetime.today(),datetime.time(19,00))
+        fixedtunnel = fixeddawn + datetime.timedelta(minutes=60)
+        scheduler.add_job(coop_open.door_run, 'date', run_date=fixeddawn)
+        scheduler.add_job(tunnel_open.door_run, 'date', run_date=fixedtunnel)
+        scheduler.add_job(coop_close.door_run, 'date', run_date=fixeddusk)
+        scheduler.add_job(tunnel_close.door_run, 'date', run_date=fixeddusk)
+        log_events('add_events DID NOT have wifi')
+
+if __name__ == '__main__':
+    #call add_events to schedule once at boot
+    add_events()
+    #schedule repeating job to schedule events at 00:10
+    scheduler.add_job(add_events, 'interval', hours=24, start_date='2021-09-26 00:10:00')
+    log_events('Startup Check')
+    while True:
+        time.sleep(60)
